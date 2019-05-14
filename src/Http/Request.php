977f228a5c97822268092308c4eb0342c35dc316
Vadim <zex404@gmail.com>
2019-05-14 21:38:04 +1000

    Request.

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,4 +5,17 @@ namespace Imhio\Http;
 class Request
 {
 
+    public $method = 'GET';
+    public $path = '/';
+    public $post = [];
+
+    public function __construct () {
+
+        $this->method = strtoupper ($_SERVER ['REQUEST_METHOD']);
+        $this->post = $_POST;
+
+        $parsed_url = parse_url ($_SERVER ['REQUEST_URI']);
+        if (isset ($parsed_url ['path']))
+            $this->path = $parsed_url ['path'];
+    }
 }
