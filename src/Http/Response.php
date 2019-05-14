1f24121ee5704fc0efe9f848cdee93417af87e86
Vadim <zex404@gmail.com>
2019-05-14 21:46:52 +1000

    Response.

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -5,4 +5,27 @@ namespace Imhio\Http;
 class Response
 {
 
+    const HTTP_OK = 200;
+    const HTTP_NOT_FOUND = 404;
+
+    public $status_code;
+    public $content;
+    public $content_type;
+
+    public function __construct (int $status_code, string $content, string $content_type) {
+
+        $this->status_code = $status_code;
+        $this->content = $content;
+        $this->content_type = $content_type;
+    }
+
+    public function render ()
+    {
+        header_remove ();
+        header ("Content-Type: {$this->content_type};charset=utf8");
+        header ("Status: {$this->status_code}");
+        http_response_code ($this->status_code);
+
+        echo $this->content;
+    }
 }
