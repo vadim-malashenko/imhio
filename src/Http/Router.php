403225072a21b0926cf9295eb7492792c5daab93
Vadim <zex404@gmail.com>
2019-05-14 21:27:33 +1000

    Pretty simple router.

--- src/Http/Router.php
+++ src/Http/Router.php
@@ -5,4 +5,25 @@ namespace Imhio\Http;
 class Router
 {
 
+    protected $request;
+    protected $routes;
+
+    public function __construct (Request $request) {
+
+        $this->request = $request;
+    }
+
+    public function add (array $routes = []) {
+
+        $this->routes = $routes;
+    }
+
+    public function route () {
+
+        foreach ($this->routes as $route)
+            if ($this->request->method == $route ['method'] && $this->request->path == $route ['path'])
+                return $route ['response'] ($this->request);
+
+        throw new NotFoundException ();
+    }
 }
